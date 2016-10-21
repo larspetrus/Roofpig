78e8970aca4d73a47c708f1c87bbc41ba192f9da
Lars <github@lar5.com>
2016-10-21 11:20:39 -0700

    Gulp can now build tests for browser

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var gulp   = require('gulp');
 var gutil  = require('gulp-util');
 var coffee = require('gulp-coffee');
@@ -5,30 +7,81 @@ var concat = require('gulp-concat');
 var cofcon = require('gulp-coffeescript-concat');
 var uglify = require('gulp-uglify');
 
+var dateFormat = require('dateformat');
+var del        = require('del');
+var filenames  = require('gulp-filenames');
+var replace    = require('gulp-replace');
 
-var rp_coffee_file = 'zz_roofpig.coffee';
-var rp_js_file = 'zz_roofpig.js';
-var extras_file = 'zz_3extras.js';
-var release_file = 'zz_roofpig_and_three.min.js';
+
+var build_dir = 'local/build/';
+var rp_js_file = 'roofpig.js';
+var extras_file = '3extras.js';
+var release_file = 'roofpig_and_three.min.js';
 var libs = "./rails_project/app/assets/javascripts/";
 
-gulp.task('build-rp', function() {
-  return gulp.src('./src/**/*.coffee')
-    .pipe(cofcon(rp_coffee_file))
-    .pipe(coffee({bare: true}).on('error', gutil.log))
-    .pipe(uglify())
-    .pipe(gulp.dest('.'));
+// ------------- BUILD -----
+
+gulp.task('clean-build', function() {
+  return del(build_dir +'**/*');
 });
 
-gulp.task('build-3extras', function() {
+gulp.task('build-rp', ['clean-build'], function() {
+  return gulp.src('./src/**/*.coffee')
+    .pipe(cofcon('roofpig.coffee'))
+    .pipe(coffee({bare: true}).on('error', gutil.log))
+    .pipe(replace('@@BUILT_WHEN@@', dateFormat(new Date(), "yyyy-mm-dd HH:MM")))
+    .pipe(uglify())
+    .pipe(gulp.dest(build_dir));
+});
+
+gulp.task('build-3extras', ['clean-build'], function() {
   return gulp.src([libs+'Projector.js', libs+'CanvasRenderer.js'])
     .pipe(uglify())
     .pipe(concat(extras_file))
-    .pipe(gulp.dest('.'));
+    .pipe(gulp.dest(build_dir));
 });
 
 gulp.task('build', ['build-rp', 'build-3extras'], function() {
-  gulp.src([libs+'three.min.js', extras_file, rp_js_file])
+  gulp.src([libs+'three.min.js', build_dir + extras_file, build_dir + rp_js_file])
     .pipe(concat(release_file))
+    .pipe(gulp.dest(build_dir));
+});
+
+
+// ------------- TEST -----
+
+var rename = require("gulp-rename");
+var glob   = require("glob");
+//var sourcemaps = require('gulp-sourcemaps');
+
+var test_js_dir = 'local/test_js/';
+
+gulp.task('clean-js', function() {
+  return del(test_js_dir +'**/*');
+});
+
+gulp.task('compile-test', ['clean-js'], function() {
+  return gulp.src('./tests/**/*.coffee')
+//    .pipe(sourcemaps.init())
+    .pipe(coffee({bare: true}).on('error', gutil.log))
+//    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(test_js_dir + 'tests'));
+});
+
+gulp.task('compile-src', ['clean-js'], function() {
+  return gulp.src('./src/**/*.coffee')
+    .pipe(coffee({bare: true}).on('error', gutil.log))
+    .pipe(gulp.dest(test_js_dir + 'src'));
+});
+
+gulp.task('test', ['compile-test', 'compile-src'], function(){
+  var test_section = '';
+  glob.sync(test_js_dir + 'tests/**/*.js', {}).forEach(function(test_file){
+    test_section += '<script src="'+test_file+'"></script>\n';
+  });
+
+  return gulp.src('./test_template.html')
+    .pipe(replace('@@TEST_FILES_GO_HERE@@', test_section))
+    .pipe(rename('rptests.html'))
     .pipe(gulp.dest('.'));
 });
